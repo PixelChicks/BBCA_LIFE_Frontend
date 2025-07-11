80daff3ea4d28c79db90abfa48329db319cb0d44
BelisAliosmanova <benidebelis@gmail.com>
2025-07-11 15:45:43 +0300

    Initial commit

---
+++ src/main/java/com/bbcalifeFrontend/bbcalifeFrontend/BbcalifeFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.bbcalifeFrontend.bbcalifeFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbcalifeFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbcalifeFrontendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bbcalifeFrontend/bbcalifeFrontend/MenuController.java
@@ -0,0 +1,16 @@
+package com.bbcalifeFrontend.bbcalifeFrontend;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class MenuController {
+
+    @GetMapping("/home")
+    public String info() {
+        return "index";
+    }
+}
+
